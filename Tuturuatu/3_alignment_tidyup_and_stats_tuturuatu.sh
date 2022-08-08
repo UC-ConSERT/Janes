e348e60948a705faa27d8e239c3affa51e0fc8a3
ojanes505 <ojanes505@gmail.com>
2022-08-09 11:23:53 +1200

    edit

--- Tuturuatu/3_alignment_tidyup_and_stats_tuturuatu.sh
+++ Tuturuatu/3_alignment_tidyup_and_stats_tuturuatu.sh
@@ -30,6 +30,7 @@ do
     #   ${sppdir}nodup_bam/${base}_nodup.bam
     samtools nodup -@ 8 ${sppdir}nodup_bam/${base}.fixmate.sorted.bam \
         ${sppdir}nodup_bam/${base}_nodup.bam
+    samtools index -@ 16 -b ${sppdir}nodup_bam/${base}_nodup.bam
     samtools stats ${bam} > ${sppdir}nodup_bam_stats/${base}.stats ####SHOULDNT this be on the nodup.bam? 
                                                                     #Rather than the original bam? Will try in tutu without overriding this already made
 done
