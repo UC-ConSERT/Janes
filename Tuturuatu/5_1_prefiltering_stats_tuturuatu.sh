9e407b63529627c8c6cb2b7bc50c1e983cccf8d9
ojanes505 <ojanes505@gmail.com>
2022-09-15 12:37:24 +1200

    editing with molly

--- Tuturuatu/5_1_prefiltering_stats_tuturuatu.sh
+++ Tuturuatu/5_1_prefiltering_stats_tuturuatu.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-#12 Aug 2022
+#12 Sep 2022
 #Adapted by Olivia Janes from https://speciationgenomics.github.io/filtering_vcfs/
 #Pre filtering statistics calculated to guide vcf filtering and compare to post-filtering statistics.
-
+##### MAY NEED TO EDIT to have output just into stats folder to make calculation later easier.
 
 sppdir=~/data/tuturuatu/
 bcfdir=${sppdir}bcf/
     #directory where files to filter are ("bcf_file" from script 4_variant_calling_filter_prep_tuturuatu.sh)
 
-mkdir -p ${bcfdir}stats/ ${bcfdir}stats/prefilter_stats/
+mkdir -p ${bcfdir}stats/
 
 varcallbcf=${bcfdir}Tuturuatu_VariantCalls_concat.bcf
     #output concatenated file from variant calling
     ##### Must be edited to be sample specific #####
     ##### Must change to bcf or vcf as previous file was #####
-outfile=${bcfdir}stats/prefilter_stats/Tuturuatu_VariantCalls_prefilter
+outfile=${bcfdir}stats/Tuturuatu_VariantCalls_prefilter
     #defining the output file prefix
 
 #If uncompressed vcf, should be compressed here using bgzip [variantcalls].vcf
@@ -26,22 +26,32 @@ wait
 
 #Calculate stats
 echo "Calculating pre-filter stats"
-    ##### Check these stats are the same as calculated afterwards #####
+
+#Calculate allele frequency for each variant
 vcftools --bcf ${varcallbcf} --freq2 --out ${outfile} --max-alleles 2
     ##### output may be changed to --gzvcf so check ######
-    #Calculate allele frequency for each variant
+    ##### Not incl in Mollys #####
+
+#Calculate mean depth per individual
 vcftools --bcf ${varcallbcf} --depth --out ${outfile}
-    #Calculate mean depth per individual
-vcftools --bcf ${varcallbcf} --site-mean-depth --out ${outfile}
-    #Calculate mean depth per site
+
+#Calculate site depth
+#####how is this different from one below? this incl in molly's. This is sum and sumsq depth.
+#####below is mean and var depth.
+vcftools --bcf ${varcallbcf} --site-depth --out ${outfile}
+
+#Calculate site quality
 vcftools --bcf ${varcallbcf} --site-quality --out ${outfile}
-    #Calculate site quality
+    ##### Not incl in Mollys #####
+    
+#Calculate proportion of missing data per individual
 vcftools --bcf ${varcallbcf} --missing-indv --out ${outfile}
-    #Calculate proportion of missing data per individual
+
+#Calculate proportion of missing data per site
 vcftools --bcf ${varcallbcf} --missing-site --out ${outfile}
-    #Calculate proportion of missing data per site
+    
+#Calculate heterozygosity
 vcftools --bcf ${varcallbcf} --het --out ${outfile}
-    #Calculate heterozygosity
 
 #To view these stats in R, see link above.
 echo "Prefilter stats has finished."
--- Tuturuatu/5_2_snp_counts_tuturuatu.sh
+++ Tuturuatu/5_2_snp_counts_tuturuatu.sh
@@ -5,10 +5,13 @@
 #Compiling all SNP counts from filtering, to compare between filtering methods.
 
 sppdir=~/data/tuturuatu/
-work=${sppdir}bcf/
+bcfdir=${sppdir}bcf/
 
+#Wouldn't this be for just one file (VariantCalls_concat.bcf) so don't need for loop? Why *vcf?
 
-for file in *.vcf 
+>>"COMMENTS"
+#This is for a vcf file: not indexed or compressed. However, may be working with a bcf file (already compressed by auto, indexed in 5_1....sh).
+for file in ${bcfdir}*.vcf 
 do 
     base=$(basename $file .vcf) 
     bgzip $file 
@@ -16,3 +19,14 @@ do
     echo $base > TLR_SNP_counts.txt 
     bcftools query -R tlr_regions.bed -f '%POS\n' ${base}.vcf.gz | wc -l > TLR_SNP_counts.txt 
 done 
+
+COMMENTS
+
+#For bcf file that was indexed in previous script (5_1_prefiltering_stats)
+for file in ${bcfdir}*VariantCalls_concat.bcf 
+do 
+    base=$(basename $file _concat.bcf)  
+    echo $base > TLR_SNP_counts.txt 
+    bcftools query -R tlr_regions.bed -f '%POS\n' ${base}.bcf | wc -l > TLR_SNP_counts.txt 
+done
+    ##### don't have tlr regions file ##### Make?
