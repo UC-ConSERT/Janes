0a344b4c376e690c9373d3c6db818ae6483812c4
ojanes505 <ojanes505@gmail.com>
2023-03-14 01:00:00 +1300

    made file from molly's github

--- Tuturuatu/5_4_filtering_strand_bias.sh
+++ Tuturuatu/5_4_filtering_strand_bias.sh
@@ -5,7 +5,7 @@
 #Filtering the "chosen" filtered bcfs, to remove any sites where strand bias adjusted phred score is <60
 
 sppdir=~/data/tuturuatu/
-finaldir=${sppdir}bcf/filter_trial/noLD/
+filterdir=${sppdir}bcf/filter_trial/noLD/
 
 mkdir -p ${sppdir}bcf_final/
 
@@ -13,12 +13,12 @@ mkdir -p ${sppdir}bcf_final/
 
 #Filtering chosen filtered file to export only sites where strand bias adjusted phred score is <60
 bcftools query -i 'FORMAT/SP<60' -f %CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO\t%FORMAT\n \
-    ${finaldir}Tuturuatu_VariantCalls_4x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf \
-    -o ${finaldir}Tuturuatu_VariantCalls_4x_coverage_0.1site_missing_MinGQ10_sp60.bcf
+    ${filterdir}Tuturuatu_VariantCalls_4x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf \
+    -o ${filterdir}Tuturuatu_VariantCalls_4x_coverage_0.1site_missing_MinGQ10_sp60.bcf
 
 bcftools query -i 'FORMAT/SP<60' -f %CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%INFO\t%FORMAT\n \
-    ${finaldir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf \
-    -o ${finaldir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10_sp60.bcf
+    ${filterdir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf \
+    -o ${filterdir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10_sp60.bcf
 
 #[%GT:PL:DP:SP:ADF:ADR:AD:GQ]
 #Error: no such tag defined in the VCF header: INFO/FORMATn
@@ -28,4 +28,4 @@ COMMENTS
     #I think Molly used just below according to her MSc Manuscript.
     #This sets individual sites with SP>60 to...?missing?
 #filter bcf file for depth and strand bias at individual sites
-bcftools +setGT ${finaldir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf -- -t q -n . -i 'FORMAT/SP>60' > ${sppdir}bcf_final/Tuturuatu_VariantCalls_final_variants.vcf.gz
+bcftools +setGT ${filterdir}Tuturuatu_VariantCalls_5x_coverage_0.1site_missing_MinGQ10.bcf.recode.bcf -- -t q -n . -i 'FORMAT/SP>60' > ${sppdir}bcf_final/Tuturuatu_VariantCalls_final_variants.vcf.gz
