8368b9f66f7a0da493f2425338c44c8fd66bce1f
ojanes505 <ojanes505@gmail.com>
2023-03-14 01:07:46 +1300

    ascultping

--- Tuturuatu_trial_2/6_haplotyping_tuturuatu_trial_2.sh
+++ Tuturuatu_trial_2/6_haplotyping_tuturuatu_trial_2.sh
@@ -5,17 +5,20 @@
 ## note to oj: find this script in Molly's github https://github.com/UC-ConSERT/Magid_et_al/blob/main/4_haplotype_creation.sh
 ##      or in manuscript (different)
 
-sppdir=~/data/tuturuatu/
+sppdir=~/data/tuturuatu_trial_2/
 finaldir=${sppdir}bcf_final/
 
 
-
+<< "COMMENTS"
 #phase each haplotype into a filtered vcf file with Beagle v. 5.2_21Apr21.304
-java -jar ##don't know what this file is:/usr/bin/beagle.28Jun21.220.jar gt=#filtered_vcf_file out=#out_vcf_file_name
+java -jar ##don't know what this file is:/usr/bin/beagle.28Jun21.220.jar gt=${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz out=tuturuatu_trial_2_phased.vcf.gz
+##OJ if using this, change all below inputs to above phased output
+
+COMMENTS
 
 #create consensus sequences with all ALT variants in a population using samtools faidx
     #this will create a file for comparison to ref genome sequence
-samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa '#tlr_region chr:start-end'| bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -M N >> ${finaldir}TLR_variants.fasta
+samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa '${sppdir}Tuturuatu_tlr_regions.bed chr:start-end'| bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -M N >> ${finaldir}TLR_variants.fasta
 
 #creates TLR haplotypes for all individuals and output sequences into a MEGA file format to use in DNAsp
 counter=1 #counter adds unique number labels to each individual for MEGA format
@@ -27,9 +30,9 @@ do
     base=$(basename $file _nodup.bam)
     echo "#"${counter}"-1" >> ${finaldir}tlr_haplotypes.meg
         #outputs sequence for phased haplotype 1 for one individual
-    samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa #tlr_region | bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -s $base -M N -H 1pIu >> ${finaldir}tlr_haplotypes.meg
+    samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa ${sppdir}Tuturuatu_tlr_regions.bed | bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -s $base -M N -H 1pIu >> ${finaldir}tlr_haplotypes.meg
     echo "#"${counter}"-2" >> ${finaldir}tlr_haplotypes.meg
         #outputs sequence for phased haplotype 2 for one individual
-    samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa #tlr_region | bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -s $base -M N -H 2pIu >> ${finaldir}tlr_haplotypes.meg
+    samtools faidx ${sppdir}ref_genome/Maui_merged_assembly.fa ${sppdir}Tuturuatu_tlr_regions.bed | bcftools consensus ${finaldir}Tuturuatu_VariantCalls_final_variants.vcf.gz -s $base -M N -H 2pIu >> ${finaldir}tlr_haplotypes.meg
     counter=$(($counter+1))
 done
