87e27ea48cbec4ff7d930db1481675cb39200490
ojanes505 <ojanes505@gmail.com>
2023-03-18 18:33:58 +1300

    etch a sketch

---
+++ Tuturuatu/3_1_extracting_stats.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e 
+
+# 17 March 2023
+# Olivia Janes
+
+# This script can be used in the home computer terminal to extract all of the outputs of the 
+#   pre- ("dup") and post- ("nodup") tidyup stats runs.
+
+cd ~/Documents/Tuturuatu_resources/tuturuatu_all/
+
+sppdir=rccuser:/home/rccuser/data/tuturuatu_all/    #define the spp directory path into the VM
+    ## Must be edited ##
+
+mkdir -p nodup_bam_stats dup_bam_stats
+mkdir -p dup_bam_stats/graphmap nodup_bam_stats/graphmap
+mkdir -p dup_bam_stats/graphmap/coverage_histograms nodup_bam_stats/graphmap/coverage_histograms
+
+homedir=~/Documents/Tuturuatu_resources/tuturuatu_all/
+nodupdir=${homedir}nodup_bam_stats/
+dupdir=${homedir}dup_bam_stats/
+
+
+##  Graphmaps   ##
+
+#   Dup graphmaps
+    rsync -rav ${sppdir}/dup_bam_stats/*graphmap ${dupdir}graphmap/  #copy dup graphmap folders into home computer
+
+    for file in ${dupdir}*.graphmap    #define file as the sample's graphmap folders
+    do 
+        base=$(basename $file _dup.graphmap)  #pull out the sample ID to be used to rename the image
+        cp $file/images_qualimapReport/*50_histogram.png ${dupdir}coverage_histograms/${base}_genome_coverage_0to50_histogram.png
+            #copy the histograms and put them in the right folder, and rename
+    done
+
+
+#   Nodup graphmaps
+    rsync -rav ${sppdir}/nodup_bam_stats/*graphmap ${nodupdir}graphmap/  #copy nodup graphmap folders into home computer
+
+    for file in ${nodupdir}*.graphmap    #define file as the sample's graphmap folders
+    do 
+        base=$(basename $file _nodup.graphmap)  #pull out the sample ID to be used to rename the image
+        cp $file/images_qualimapReport/*50_histogram.png ${nodupdir}coverage_histograms/${base}_genome_coverage_0to50_histogram.png
+            #copy the histograms and put them in the right folder, and rename
+    done
---
+++ Tuturuatu_all/2_1_pretidyup_stats_tuturuatu_all.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e 
+
+#21 July 2022
+#Tuturuatu alignment stats script adapted from https://github.com/janawold1/PhD_thesis/blob/main/Chapter_4/3_align_stats.md
+#To be used before running tidyup on the merged bam, and compared to the stats created after tidyup.
+#Jana Wold, adapted by Olivia Janes
+
+#####   Must be edited to be run specific   #####
+sppdir=~/data/tuturuatu_all/
+
+mergedbamdir=${sppdir}merged_bam_files/
+    #directory that holds the aligned, sorted and merged bam files
+
+mkdir -p ${sppdir}dup_bam_stats/
+    #Making directories to hold the merged bam stats generated.
+
+#Samtools stats are run for merged/aligned bam files..
+for bam in ${mergedbamdir}*_merged.bam
+do
+    base=$(basename ${bam} _merged.bam)
+    echo "Now preparing to create stats for ${base}..."
+    samtools stats ${bam} > ${sppdir}dup_bam_stats/${base}_merged_dup.stats
+done
+echo "Samtools stats has finished. Time to run stats, stat!"
+
+#Running mosdepth and qualimap stats to observe the merged bam files.
+for bam in ${mergedbamdir}*_merged.bam
+    do
+    base=$(basename ${bam} _merged.bam)
+    echo "Running Qualimap for ${base}..."
+    qualimap bamqc \
+        -bam ${bam} \
+        -nw 10000 \
+        -nt 16 -c \
+        -outdir ${sppdir}dup_bam_stats/${base}_merged_dup.graphmap \
+        --java-mem-size=8G
+    echo "Running calculating stats for ${base}..."
+    mosdepth --threads 24 --fast-mode --by 50 ${sppdir}dup_bam_stats/${base} ${bam}
+done
+echo "Stats have finished."
+
+#Plotting mosdepth outputs.
+python ~/data/general_scripts/plot-dist.py ${sppdir}dup_bam_stats/*.global.dist.txt > ${sppdir}dup_bam_stats/dup_bam_plot_dist_tutu_all.html
+
+echo "Script has finished."
