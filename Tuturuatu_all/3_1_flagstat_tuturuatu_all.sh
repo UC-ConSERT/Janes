3483f4d67b3381b72a5315eb52e9059b9c601616
ojanes505 <ojanes505@gmail.com>
2023-03-19 00:03:13 +1300

    country boy

--- Tuturuatu_all/3_1_flagstat_tuturuatu_all.sh
+++ Tuturuatu_all/3_1_flagstat_tuturuatu_all.sh
@@ -14,14 +14,22 @@ echo "Running flagstat on dup bams"
 for bam in ${mergedbamdir}*_merged.bam
 do
     echo "Running flagstat on ${bam}"
-    samtools flagstat $bam >> ${sppdir}dup_bam_stats/flagstat_dup.txt
+    base=$(basename ${bam} _merged.bam)
+    echo "##    ${base} dup bam" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+        echo "" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+    samtools flagstat $bam >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
 done
 
 echo "Running flagstat on nodup bams"
 for bam in ${sppdir}nodup_bam/*_nodup.bam
 do
     echo "Running flagstat on ${bam}"
-    samtools flagstat $bam >> ${sppdir}nodup_bam_stats/flagstat_nodup.txt
+    base=$(basename ${bam} _nodup.bam)
+    echo "" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+        echo "" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+        echo "##    ${base} NOdup bam" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+        echo "" >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
+    samtools flagstat $bam >> ${sppdir}nodup_bam_stats/${base}_flagstat.txt
 done
 
 echo ""
