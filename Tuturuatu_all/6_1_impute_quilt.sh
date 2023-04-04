fbfb91abb747888bcada54d6550b8cad6642e5d1
ojanes505 <ojanes505@gmail.com>
2023-04-05 02:19:45 +1200

    co,mmit

--- Tuturuatu_all/6_1_impute_quilt.sh
+++ Tuturuatu_all/6_1_impute_quilt.sh
@@ -2,12 +2,12 @@
 
 # 04 April 2023
 # Olivia Janes
-# Imputing with QUILT
+# Imputing with QUILT - a reference panel based imputation
 # Imputing tuturuatu_all bcf to improve SNP calling in low coverage (<5x) individuals.
 # Imputation will be on the variant call bcf that has been filtered for 
 #   4x coverage, 0.2 site missingness, 10 minGQ and no LD.
 
-#Environment: stitch
+#Environment: quilt
 
 sppdir=~/data/tuturuatu_all/
 ## Edit to be run specific
@@ -16,6 +16,7 @@ sppdir=~/data/tuturuatu_all/
 impdir=${sppdir}impute/
 subsetdir=${impdir}bcf_subsets/
 
-# Activating STITCH (required for QUILT)
+# Activating STITCH (required for QUILT) & QUILT
 R -e 'library("STITCH")'
+R -e 'library("QUILT")'
 ## Is this necessary?
