2cd841d37d9f3911bf9bbc8974459166e53982db
ojanes505 <ojanes505@gmail.com>
2023-04-10 21:46:54 +1200

    ?

--- Tuturuatu/5_9_downloading_stats.sh
+++ Tuturuatu/5_9_downloading_stats.sh
@@ -6,15 +6,18 @@
 # A script to download all of the stats files from the VM to the home computer
 ## Run this from the home computer directory for the species and run, for example: ~/Documents/Tuturuatu_resources/tuturuatu_all_rm_bad/
 
-spprun=tuturuatu_all_rm_a09
+spprun=tuturuatu_all_vcf
 ##  Needs to be edited to be species and run specific
 
 #Setting up
+    cd ~/Documents/Tuturuatu_resources/
+    mkdir ${spprun}
     cd ~/Documents/Tuturuatu_resources/${spprun}
     mkdir -p filter_stats
     mkdir -p filter_stats/stats_raw_files filter_stats/tlr_stats filter_stats/strand_bias_filter_stats
     mkdir -p filter_stats/strand_bias_filter_stats/tlr_stats filter_stats/strand_bias_filter_stats/sb_stats_raw_files
 
+<<"COMMENTS"
 #Download filter stats
     echo "Downloading filter stats"
     cd ./filter_stats/
@@ -23,8 +26,8 @@ spprun=tuturuatu_all_rm_a09
     rsync -rav rccuser:/home/rccuser/data/${spprun}/bcf/stats/TLR_SNP_counts.txt ./
     rsync -rav rccuser:/home/rccuser/data/${spprun}/bcf/stats/tlr_stats/* ./tlr_stats/
     echo ""
+COMMENTS
 
-<<"COMMENTS"
 #Download strand bias filter stats
     echo "Downloading strand bias filter stats"
     cd ./strand_bias_filter_stats/
@@ -33,5 +36,5 @@ spprun=tuturuatu_all_rm_a09
     rsync -rav rccuser:/home/rccuser/data/${spprun}/bcf/strand_bias_filter_stats/TLR_SNP_counts_strand_bias.txt ./
     rsync -rav rccuser:/home/rccuser/data/${spprun}/bcf/strand_bias_filter_stats/tlr_stats/* ./tlr_stats/
     echo ""
-COMMENTS
+
 echo "Script complete."
