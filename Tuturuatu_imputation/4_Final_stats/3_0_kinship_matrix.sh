a481b5ec9cc2489ad0dacf67e37f35484f6d4dcd
ojanes505 <ojanes505@gmail.com>
2023-05-17 14:38:29 +1200

    toodle-oo

--- Tuturuatu_imputation/4_Final_stats/3_0_kinship_matrix.sh
+++ Tuturuatu_imputation/4_Final_stats/3_0_kinship_matrix.sh
@@ -16,7 +16,7 @@ set -e
 
 
 #Setting directories
-    mkdir -p ${sppdir}final_outputs/final_stats/kinship
+    mkdir -p ${sppdir}final_outputs/final_stats/kinship/wgs/
     statsdir=${sppdir}final_outputs/final_stats/kinship/
     workdir=${sppdir}final_outputs/final_vcfs/
 
@@ -31,8 +31,23 @@ set -e
         plink2 --vcf ${file} --make-king-table --allow-extra-chr --out ${statsdir}${base}_kinship >> ${statsdir}${base}_kinship.log
     done
 
+    for file in ${sppdir}bcf/filter_trial/impute/*5x*vcf.gz
+    do
+        base=$(basename ${file} .vcf.gz)
+        echo "Making kinship matrix for ${base}"
+        plink2 --vcf ${file} --make-king --allow-extra-chr --out ${statsdir}wgs/${base}_kinship > ${statsdir}wgs/${base}_kinship.log
+        echo "Making kinship table for ${base}"
+        plink2 --vcf ${file} --make-king-table --allow-extra-chr --out ${statsdir}wgs/${base}_kinship >> ${statsdir}wgs/${base}_kinship.log
+    done
 
-
+    for file in ${sppdir}bcf/filter_trial/noLD/*_VariantCalls_5x_coverage_0.2site_missing_MinGQ10.vcf.gz
+    do
+        base=$(basename ${file} .vcf.gz)
+        echo "Making kinship matrix for ${base}"
+        plink2 --vcf ${file} --make-king --allow-extra-chr --out ${statsdir}wgs/${base}_kinship > ${statsdir}wgs/${base}_kinship.log
+        echo "Making kinship table for ${base}"
+        plink2 --vcf ${file} --make-king-table --allow-extra-chr --out ${statsdir}wgs/${base}_kinship >> ${statsdir}wgs/${base}_kinship.log
+    done
 #Downloading stats to home computer. Navigate to final_outputs folder.
 #mkdir -p final_stats/kinship/
 #rsync -rav rccuser:/home/rccuser/data/tuturuatu_all_vcf/final_outputs/final_stats/kinship/* final_stats/kinship/
